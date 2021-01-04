224f672395625de777c51a63d5443a6151aca35e
chetagra <chetanagrawalstar1999@gmail.com>
2021-01-04 14:42:05 +0530

    trycatch added

--- src/db/model.js
+++ src/db/model.js
@@ -8,41 +8,57 @@ const cartSchema = new mongoose.Schema({
     manufacturer: String
 })
 
-const cart =   mongoose.model('cart',cartSchema)
+const cart = mongoose.model('cart',cartSchema)
 
 const insert = async ()=>{
-    const items = await cart.create({
-        productName : "Realme-X",
-        price : 20000,
-        manufacturer : "Realme"
-    },
-    {
-    productName : "Samsung-S10",
-    price : 90000,
-    manufacturer : "Samsung"
-    },
-    {
-        productName : "Iphone-11",
-        price : 60000,
-        manufacturer : "Apple"
-    },
-    {
-        productName : "Oneplus-7",
-        price : 30000,
-        manufacturer : "Oneplus"
-    })
+    try {
 
-    console.log(items);
+        const items = await cart.create({
+            productName : "Realme-X",
+            price : 20000,
+            manufacturer : "Realme"
+        },
+        {
+        productName : "Samsung-S10",
+        price : 90000,
+        manufacturer : "Samsung"
+        },
+        {
+            productName : "Iphone-11",
+            price : 60000,
+            manufacturer : "Apple"
+        },
+        {
+            productName : "Oneplus-7",
+            price : 30000,
+            manufacturer : "Oneplus"
+        })
+    
+        console.log(items);
+        
+    } catch (e) {
+        console.error(e);
+    }
+   
     
 }
 
 
 const read=async ()=>{
-    const items= await cart.find({})
+
+    try {
+
+        const items= await cart.find({})
      
-    items.forEach(element => {
-        console.log(element)
-    })
+        items.forEach(element => {
+            console.log(element)
+        })
+        
+    } catch (e) {
+        console.error(e);
+    }
+
+ 
 }
 
 
